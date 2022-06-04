72944040d5b5f233ccfed1c7f07adac82d296da5
EstebanIbarra <sectorjeat@gmail.com>
2022-06-04 13:05:03 -0500

    Adds DryWet feature

---
+++ Source/DryWet.cpp
@@ -0,0 +1,27 @@
+/*
+  ==============================================================================
+
+    DryWet.cpp
+    Created: 28 May 2022 1:41:28pm
+    Author:  eir
+
+  ==============================================================================
+*/
+
+#include "DryWet.h"
+
+DryWet::DryWet(){};
+DryWet::~DryWet(){};
+
+void DryWet::process(juce::AudioBuffer<float> &inputBufferWet, juce::AudioBuffer<float> &inputBufferDry, float inputDryWet)
+{
+    inputDryWet /= 100.0f;
+    for(int channel = 0; channel < inputBufferWet.getNumChannels(); channel++) {
+        for (int sampleIndex = 0; sampleIndex < inputBufferWet.getNumSamples(); sampleIndex++) {
+            float wetSample = inputBufferWet.getSample(channel, sampleIndex);
+            float drySample = inputBufferDry.getSample(channel, sampleIndex);
+            float outputSample = drySample * (1.0f - inputDryWet) + (wetSample * inputDryWet);
+            inputBufferWet.setSample(channel, sampleIndex, outputSample);
+        }
+    }
+}
---
+++ Source/DryWet.h
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    DryWet.h
+    Created: 28 May 2022 1:41:28pm
+    Author:  eir
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class DryWet
+{
+public:
+    
+    DryWet();
+    ~DryWet();
+    
+    /**
+     Mixes the dry and wet signal with the specified relation
+     @param juce::AudioBuffer<float> &inputBufferWet
+     @param juce::AudioBuffer<float> &inputBufferDry
+     @param float inputDryWet
+     */
+    void process(juce::AudioBuffer<float> &inputBufferWet, juce::AudioBuffer<float> &inputBufferDry, float inputDryWet = 50.0f);
+};
