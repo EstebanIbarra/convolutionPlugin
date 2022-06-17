0be6c692391ab88c19ee22a13cc15101b97c1f46
EstebanIbarra <sectorjeat@gmail.com>
2022-06-17 00:30:00 -0500

    Adds File Manager

---
+++ Source/FileManager.cpp
@@ -0,0 +1,46 @@
+/*
+  ==============================================================================
+
+    FileManager.cpp
+    Created: 16 Jun 2022 11:02:11pm
+    Author:  eir
+
+  ==============================================================================
+*/
+
+#include "FileManager.h"
+
+FileManager::FileManager()
+{
+    createFolderStructure();
+}
+
+FileManager::~FileManager()
+{
+    applicationDataFolder = "";
+}
+
+void FileManager::createFolderStructure()
+{
+    setApplicationDataFolder(juce::File::getSpecialLocation(juce::File::userApplicationDataDirectory).getFullPathName());
+    for (juce::String dir : dirs) {
+        setApplicationDataFolder(juce::File::addTrailingSeparator(applicationDataFolder.getFullPathName()) + dir);
+    }
+    if (!applicationDataFolder.isDirectory()) applicationDataFolder.createDirectory();
+}
+
+void FileManager::copyFileToApplicationData (const juce::String &absolutePath)
+{
+    juce::File file = absolutePath;
+    file.copyFileTo(applicationDataFolder.getFullPathName());
+}
+
+void FileManager::setApplicationDataFolder(const juce::String &absolutePath)
+{
+    applicationDataFolder = absolutePath;
+}
+
+juce::File FileManager::getApplicationDataFolder()
+{
+    return applicationDataFolder;
+}
---
+++ Source/FileManager.h
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    FileManager.h
+    Created: 16 Jun 2022 11:02:11pm
+    Author:  eir
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class FileManager
+{
+public:
+    FileManager();
+    ~FileManager();
+    
+    /**
+     Creates folder structure
+     @return void
+     */
+    void createFolderStructure();
+    
+    /**
+     Copies a file to the Application Data folder
+     @param juce::String &absolutePath
+     @return void
+     */
+    void copyFileToApplicationData (const juce::String &absolutePath);
+    
+    /**
+     Sets the full path for the Application Data Folder
+     @param juce::String &absolutePath
+     @return void
+     */
+    void setApplicationDataFolder(const juce::String &absolutePath);
+    
+    /**
+     Get the path of the Application Data Folder
+     @return juce::File
+     */
+    juce::File getApplicationDataFolder();
+    
+private:
+    juce::File applicationDataFolder;
+    juce::StringArray dirs {
+        JucePlugin_Name,
+        "External",
+        "IR"
+    };
+};
